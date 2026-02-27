9a7991ec7f16a2b6dbac5fff662ca2ead0d854d4
vtu24625-maker <vtu24625@veltech.edu.in>
2026-02-27 10:36:30 +0530

    Time: 0 ms (100%), Space: 48.6 MB (12.62%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid; // target found
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // target not found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println(search(nums, target)); // Output: 4
+    }
+}
